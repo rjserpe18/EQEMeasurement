77ce95532095d72824bcda7961b13e7de1f018fa
rjserpe18 <rexserpe@gmail.com>
2019-06-07 13:42:04 -0400

    refining the library

--- MonoChromater.py
+++ MonoChromater.py
@@ -1,24 +1,75 @@
 import visa
 import pymeasure
+import time
 
 rm = visa.ResourceManager()
-print(rm.list_resources())
 
 MonoChromater = rm.open_resource('GPIB0::1::INSTR')
 
+#i3 -- slit with output light
 
-print(MonoChromater.write("g0,0,400"))
-
-print(MonoChromater.write("k0,1,-500"))
-
-
-
-#make sure to delay for 100 seconds after waking up computer
+#32 steps changes by 1 nanometer
+#it also waits up on the command
+#to get something to move the way you want it to, just precede it by the zero command
 
 
+MonoChromater.write("F0,1000")
+MonoChromater.write("F0,0")
+
+time.sleep(5)
+
+MonoChromater.write("F0,-1000")
+MonoChromater.write("F0,0")
+
+
+
+
+
+
+'''
+
+512.50
+509.38
+506.25
+503.13
+500
+
+
+
+
+
+print("initializing the motor")
+time.sleep(10)
+
+MonoChromater.write('i0,3,0')
+
+print("initializing the slit ")
+time.sleep(10)
+
+
+MonoChromater.write("g0,3,5000")
+
+
+print("setting the speed")
+time.sleep(10)
+
+
+MonoChromater.write('k0,3,1000')
+
+
+print("opening the front slit")
+'''
 
 #
+# print(MonoChromater.write("g0,0,400"))
 #
+#
+# print(MonoChromater.write("k0,1,-500"))
+
+
+
+
+
 # print(MonoChromater.write(" "))
 #
 #
--- powerMeter.py
+++ powerMeter.py
@@ -1,6 +1,6 @@
 import visa
 import time
-
+import statistics
 
 class PowerMeter:
 
@@ -8,9 +8,11 @@ class PowerMeter:
 
     def __init__(self,address,nickName):
 
+        rm = visa.ResourceManager()
+
         powerMeter = rm.open_resource(address)
 
-        self.resourceManager = visa.ResourceManager()
+        self.resourceManager = rm
 
         self.powerMeter = powerMeter
 
@@ -46,27 +48,68 @@ class PowerMeter:
         power_W = float(self.powerMeter.query('measure:power?'))
         return power_W
 
+    def measurePowerUntilSteady(self):
+
+        def deviationIsSteady(deviation):
+            while (deviation > 1):
+                return False
+            return True
+
+        def percentSTDV(dataSet):
+            return 100 * statistics.stdev(dataSet) / statistics.mean(dataSet)
+
+        data = []
+
+        i = 1
+
+        deviation = 1.1
+
+        while (deviationIsSteady(deviation) == False):
+
+            print("")
+            print("trial " + str(i) + " at wavelength: " + str(thorLabs.getWavelength()))
+
+            data = []
+
+            print("measuring values")
+            for j in range(1, 6, 1):
+                data.append(thorLabs.getPower())
+                time.sleep(1)
+
+            print("assessing data stability...")
+            time.sleep(2)
+
+            deviation = percentSTDV(data)
+            print("stdv for trial " + str(i) + ": " + str(deviation))
+
+            print("data set for trial " + str(i) + ":" + str(data))
+
+            if (deviation > 1):
+                print("data unsteady. remeasuring....")
+            else:
+                print("data steady!")
+
+            i += 1
+
+        print(str(statistics.mean(data))+(" W"))
+
+
+
+thorLabs = PowerMeter('USB0::0x1313::0x8072::P2003101::0::INSTR', "thor labs")
+
+thorLabs.setWavelength(638)
+
+thorLabs.measurePowerUntilSteady()
 
 
 
 
-#this is the code we used to get those data points
 
-# rm = visa.ResourceManager()
-#
-# thorLabs = PowerMeter('USB0::0x1313::0x8072::P2003101::0::INSTR', "thor labs")
-#
-# timeVals = []
-# powerVals =[]
-#
-#
-# thorLabs.setWavelength(638)
-#
-#
-# for i in range (0,3600,30):
-#     timeVals.append(i)
-#     powerVals.append(thorLabs.getPower())
-#     print(thorLabs.getPower())
-#     print(i/60)
-#
-#     time.sleep(30)
+
+
+
+
+
+
+
+
