ab97bee9027f1ad42467c6fd4739471264df8932
SarcasticStarch <51371910+SarcasticStarch@users.noreply.github.com>
2019-06-28 15:13:17 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-subprocess.call(['python','get_pip.py'])
-subprocess.call([sys.executable, '-m', 'pip', 'install', '{0}=={1}'.format('numpy', '1.15.4')])
-subprocess.call([sys.executable, '-m', 'pip', 'install', '{0}=={1}'.format('matplotlib', '3.1.0')])
-subprocess.call([sys.executable, '-m', 'pip', 'install', '{0}=={1}'.format('seaborn', '0.9.0')])
-
